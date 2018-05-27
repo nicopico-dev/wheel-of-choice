2e98823c2788a53b5cffe2ec4a743ee863103d72
Nicolas Picon <nicopico.dev@gmail.com>
2018-05-27 18:07:53 +0200

    Apply settings change to the choice wheel

--- lib/choice_wheel.dart
+++ lib/choice_wheel.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:wheel_of_choice/data.dart';
 
-class ChoiceWheel extends StatelessWidget {
+class ChoiceWheel extends StatefulWidget {
   const ChoiceWheel({this.choices});
-
   final ChoiceData choices;
 
   @override
-  Widget build(BuildContext context) => ListView.builder(
-        itemCount: choices.size,
-        itemBuilder: (context, index) => _createSection(choices[index]),
-      );
+  ChoiceWheelState createState() {
+    return new ChoiceWheelState();
+  }
 
   _createSection(Choice choice) {
     return Container(
       child: ListTile(title: Text(choice.name)),
       color: choice.color,
     );
+  }
+}
+
+class ChoiceWheelState extends State<ChoiceWheel> {
+  @override
+  void initState() {
+    super.initState();
+    widget.choices.addListener(refresh);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.choices.size,
+        itemBuilder: (context, index) =>
+            widget._createSection(widget.choices[index]),
+      );
+  }
+
+  @override
+  void dispose() {
+    widget.choices.removeListener(refresh);
+    super.dispose();
+  }
+
+  void refresh() {
+    setState(() {});
   }
 }
